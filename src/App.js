83520551c37b0cd4e96ac516fb0df26c45d7a194
alex-bilyk <bilyksaha01@gmail.com>
2020-08-04 00:11:36 +0300

    Added basic exapmle using bootstrap UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,125 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import FeatherIcon from 'feather-icons-react'
+
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+                <a className="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href={'#'}>Company name</a>
+
+                <button className="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <input className="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search" />
+
+                <ul className="navbar-nav px-3">
+                    <li className="nav-item text-nowrap">
+                        <a className="nav-link" href={'#'}>Sign out</a>
+                    </li>
+                </ul>
+            </nav>
+
+            <div className="container-fluid">
+                <div className="row">
+                    <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+                        <div className="sidebar-sticky pt-3">
+                            <ul className="nav flex-column">
+                                <li className="nav-item">
+                                    <a className="nav-link active" href={'#'}>
+                                        <FeatherIcon icon="home" />
+                                        Dashboard <span className="sr-only">(current)</span>
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href={'#'}>
+                                        <FeatherIcon icon="bar-chart-2" />
+                                        Reports
+                                    </a>
+                                </li>
+                            </ul>
+
+                            <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                                <span>Saved reports</span>
+
+                                <a className="d-flex align-items-center text-muted" href={'#'} aria-label="Add a new report">
+                                    <FeatherIcon icon="plus-circle" />
+                                </a>
+                            </h6>
+
+                            <ul className="nav flex-column mb-2">
+                                <li className="nav-item">
+                                    <a className="nav-link" href={'#'}>
+                                        <FeatherIcon icon="file-text" />
+                                        Current month
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href={'#'}>
+                                        <FeatherIcon icon="file-text" />
+                                        Last quarter
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+
+                    <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+                        <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                            <h1 className="h2">Dashboard</h1>
+
+                            <div className="btn-toolbar mb-2 mb-md-0">
+                                <div className="btn-group mr-2">
+                                    <button type="button" className="btn btn-sm btn-outline-secondary">Share</button>
+                                    <button type="button" className="btn btn-sm btn-outline-secondary">Export</button>
+                                </div>
+
+                                <button type="button" className="btn btn-sm btn-outline-secondary dropdown-toggle">
+                                    <FeatherIcon icon="calendar" />
+                                    This week
+                                </button>
+                            </div>
+                        </div>
+
+                        <h2>Section title</h2>
+
+                        <div className="table-responsive">
+                            <table className="table table-striped table-sm">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Header</th>
+                                        <th>Header</th>
+                                        <th>Header</th>
+                                        <th>Header</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1,001</td>
+                                        <td>Lorem</td>
+                                        <td>ipsum</td>
+                                        <td>dolor</td>
+                                        <td>sit</td>
+                                    </tr>
+                                    <tr>
+                                        <td>1,002</td>
+                                        <td>amet</td>
+                                        <td>consectetur</td>
+                                        <td>adipiscing</td>
+                                        <td>elit</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </main>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
